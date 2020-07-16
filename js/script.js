1f73842bf3da87fbdd71d040b081a114ee828a59
Vaibhav Saini <32795689+VaibhavSaini19@users.noreply.github.com>
2020-07-16 10:42:18 +0530

    Add fade and switch iframe functionality

--- js/script.js
+++ js/script.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
 	$("#sidebarCollapse").on("click", function() {
 		$("#sidebar").toggleClass("active");
 	});
+
+	$("#sidebar .nav .nav-link").on("click", (e) => {
+		let ele = e.target.closest("a");
+		let type = $(ele).attr("data-block-type");
+		let num = $(ele).attr("data-block-num");
+		let newSrc = `../blocks/${type}/${type}_${num}.html`;
+		let iframe = $("#blocks-blog-1 iframe");
+		iframe.fadeOut(200, () => {
+			iframe.attr("src", newSrc);
+			iframe.fadeIn(200);
+		});
+	})
 });
