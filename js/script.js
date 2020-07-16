95da9bfb243cddcbdb2e431f4435875bb07d8c5a
Vaibhav Saini <32795689+VaibhavSaini19@users.noreply.github.com>
2020-07-16 13:00:07 +0530

    Switch to overlay sidebar

--- js/script.js
+++ js/script.js
@@ -3,12 +3,24 @@ $(document).ready(function() {
 		$("#sidebar").toggleClass("active");
 	});
 
+	$('#dismiss, .overlay').on('click', function () {
+		$('#sidebar').removeClass('active');
+		$('.overlay').removeClass('active');
+	});
+
+	$('#sidebarCollapse').on('click', function () {
+		$('#sidebar').addClass('active');
+		$('.overlay').addClass('active');
+		$('.collapse.in').toggleClass('in');
+		$('a[aria-expanded=true]').attr('aria-expanded', 'false');
+	});
+
 	$("#sidebar .nav .nav-link").on("click", (e) => {
 		let ele = e.target.closest("a");
 		let type = $(ele).attr("data-block-type");
 		let num = $(ele).attr("data-block-num");
 		let newSrc = `../blocks/${type}/${type}_${num}.html`;
-		let iframe = $("#blocks-blog-1 iframe");
+		let iframe = $("#blocks-container iframe");
 		iframe.fadeOut(200, () => {
 			iframe.attr("src", newSrc);
 			iframe.fadeIn(200);
