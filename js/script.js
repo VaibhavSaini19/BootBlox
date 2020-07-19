f0d33f423e41cadc337ba4a4778a18e44d312ac1
Vaibhav Saini <32795689+VaibhavSaini19@users.noreply.github.com>
2020-07-19 13:02:13 +0530

    Fix Line number append issue

--- js/script.js
+++ js/script.js
@@ -36,9 +36,8 @@ $(document).ready(function() {
 	function showHideCode(ele){
 		let iframe = $("#blocks-iframe");
 		let codeDiv = $("#blocks-code");
-		let lineCountDisplay = $("#line-count-display");
+		let pre = document.querySelector('pre');
 		if(ele.hasClass("active")){
-			// let srcCode = iframe.contents()[0];
 			let srcCode = iframe.contents().find("section").get(0).outerHTML;
 			// console.log(srcCode);
 			var mapObj = {
@@ -51,23 +50,18 @@ $(document).ready(function() {
 			});
 			// console.log(srcCode);
 			iframe.fadeOut(100, () => {
-				codeDiv.find("code").append(srcCode);
+				codeDiv.find("code").html(srcCode);
 				ele.find("span").html("Preview");
 				ele.find("i.fas").removeClass("fa-code").addClass("fa-eye");
 				codeDiv.fadeIn(100);	
 				document.querySelectorAll('pre code').forEach((block) => {
 					hljs.highlightBlock(block);
 				});
-				let pre = document.getElementsByTagName('pre'),
-					pl = pre.length;
-				for (let i = 0; i < pl; i++) {
-					pre[i].innerHTML = '<span class="line-number"></span>' + pre[i].innerHTML + '<span class="cl"></span>';
-					let num = pre[i].innerHTML.split(/\n/).length;
-					console.log(num);
-					for (let j = 0; j < num; j++) {
-						let line_num = pre[i].getElementsByTagName('span')[0];
-						line_num.innerHTML += '<span>' + (j + 1) + '</span>';
-					}
+				let num = pre.innerHTML.split(/\n/).length;
+				let line_num = pre.getElementsByTagName('span')[0];
+				line_num.innerHTML = "";
+				for (let j = 0; j < num; j++) {
+					line_num.innerHTML += '<span>' + (j + 1) + '</span>';
 				}
 			});
 		}else{
