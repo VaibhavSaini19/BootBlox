4549061c2058302dbd9b71dcf50b4d2468484b7c
Vaibhav Saini <32795689+VaibhavSaini19@users.noreply.github.com>
2020-07-19 09:31:24 +0530

    Add functionality to preview source code

--- js/script.js
+++ js/script.js
@@ -16,10 +16,45 @@ $(document).ready(function() {
 		let type = $(ele).attr("data-block-type");
 		let num = $(ele).attr("data-block-num");
 		let newSrc = `../blocks/${type}/${type}_${num}.html`;
-		let iframe = $("#blocks-container iframe");
+		let iframe = $("#blocks-iframe");
 		iframe.fadeOut(200, () => {
 			iframe.attr("src", newSrc);
 			iframe.fadeIn(200);
 		});
+		let previewBtn = $("#code-preview");
+		previewBtn.removeClass("active");
+		showHideCode(previewBtn);
 	})
+
+	$("#blocks-code").hide();
+	$("#code-preview").on("click", (e) => {
+		let ele = $(e.target);
+		ele.toggleClass("active");
+		showHideCode(ele);
+	})
+
+	function showHideCode(ele){
+		let iframe = $("#blocks-iframe");
+		let codeDiv = $("#blocks-code");
+		if(ele.hasClass("active")){
+			// let srcCode = iframe.contents()[0];
+			let srcCode = iframe.contents().find("section")[0].outerHTML;
+			srcCode = srcCode.replace(/[\u00A0-\u9999<>\&]/gim, (i) => {
+				return '&#'+i.charCodeAt(0)+';';
+			});
+			// console.log(srcCode);
+			iframe.fadeOut(100, () => {
+				codeDiv.find("code").html(srcCode);
+				ele.find("span").html("Preview");
+				ele.find("i.fas").removeClass("fa-code").addClass("fa-eye");
+				codeDiv.fadeIn(100);	
+			});
+		}else{
+			codeDiv.fadeOut(100, () => {
+				ele.find("span").html("View Code");
+				ele.find("i.fas").removeClass("fa-eye").addClass("fa-code");
+				iframe.fadeIn(100);
+			});
+		}
+	}
 });
