37d492787d9b23440fa663ee855e6d247270a27b
Vaibhav Saini <32795689+VaibhavSaini19@users.noreply.github.com>
2020-07-29 17:53:42 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,8 +31,8 @@ $(document).ready(function() {
 				updateTheme();
 				setTimeout(() => {
 					iframe.animate({opacity: 1}, fadeDelay);
-				}, 100);
-			}, 100);
+				}, fadeDelay);
+			}, fadeDelay*1.5);
 		})
 	})
 
